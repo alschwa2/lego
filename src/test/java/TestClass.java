32edafb2f2130ceb747b32a3ca2949d30f960bc4
nopo12 <noah.potash@mail.yu.edu>
2018-05-14 18:35:38 -0400

    test_class

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+package java;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+
+public class TestClass{
+
+    @Test
+    public void testMethod() {
+        assertThat("a", containsString("Hello"));
+    }
+
+}
